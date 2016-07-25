c2cf750ff2ec4d35edc1e3a36c454867309f9ff6
Meaghan Jones <meaghan.m.jones@gmail.com>
2016-07-25 11:22:35 -0700

    pass DateIdea spec to validate length of name

--- spec/date_idea_spec.rb
+++ spec/date_idea_spec.rb
@@ -5,4 +5,14 @@ describe(DateIdea)do
     date_idea = DateIdea.new({:name => ''})
     expect(date_idea.save()).to(eq(false))
   end
+  it('validates length of name is less than 50 characters') do
+    date_idea = DateIdea.create({:name => 'a'.*(51)})
+    expect(date_idea.save()).to(eq(false))
+  end
+  describe('Store#titlecase_name') do
+      it('converts the name of the date to title case') do
+        date_idea = DateIdea.create({:name => 'bowling alley'})
+        expect(date_idea.name()).to(eq('Bowling Alley'))
+      end
+  end
 end
