e244b566c2f22ecabe5aa70500c0749182111a79
Meaghan Jones <meaghan.m.jones@gmail.com>
2016-07-25 14:36:07 -0700

    pass date idea pages integration spec add a new date to the database

---
+++ spec/date_idea_pages_spec.rb
@@ -0,0 +1,15 @@
+require('spec_helper')
+
+describe 'Add a date path', {:type => :feature} do
+  it 'allows users to add a new date to the database' do
+    visit '/'
+    click_link 'Add a New Date'
+    fill_in 'name', :with => 'Ikea'
+    fill_in 'street', :with => '10280 NE Cascades Parkway'
+    fill_in 'city', :with => 'Portland'
+    fill_in 'state', :with => 'OR'
+    fill_in 'description', :with => 'Eat meatballs and sit on couches!'
+    click_button 'Add Date'
+    expect(page).to have_content('Ikea')
+  end
+end
