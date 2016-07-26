5ba078a2a27b8a5e6b36b8642dc2a25e4a060872
Stephanie Gurung <stephr3@gmail.com>
2016-07-26 09:07:31 -0700

    pass Tag#titlecase_tag_name spec

--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -5,4 +5,14 @@ describe(Tag) do
     tag = Tag.new({:name => ''})
     expect(tag.save).to(eq(false))
   end
+  it('validates the length of the tag is less than 20 characters') do
+    tag = Tag.create(:name => 'a'.*(21))
+    expect(tag.save).to(eq(false))
+  end
+  describe('Tag#titlecase_tag_name') do
+    it('converts the tag name to title case') do
+      tag = Tag.create({:name => 'super romantic' })
+      expect(tag.name).to(eq('Super Romantic'))
+    end
+  end
 end
