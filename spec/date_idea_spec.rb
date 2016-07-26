6876cdbe628aee3c7ed31b6160fb6989f339a59b
Stephanie Gurung <stephr3@gmail.com>
2016-07-26 11:14:31 -0700

    pass dateIdea spec: belongs to a tag

--- spec/date_idea_spec.rb
+++ spec/date_idea_spec.rb
@@ -9,6 +9,11 @@ describe DateIdea do
     date_idea = DateIdea.create({:name => 'a'.*(51)})
     expect(date_idea.save()).to(eq(false))
   end
+  it('belongs to a tag')do
+    tag = Tag.create({:name => 'Romantic'})
+    date_idea = tag.date_ideas().new({:name => 'Candle Lit Dinner'})
+    expect(tag.date_ideas()).to(eq([date_idea]))
+  end
   describe 'DateIdea#titlecase_name' do
       it 'converts the name of the date to title case' do
         date_idea = DateIdea.create({:name => 'bowling alley'})
